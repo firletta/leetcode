0906544f216862fad1ec26d8d539c275fe6612b0
firletta <firletta@gmail.com>
2024-01-24 19:30:55 +0100

    replace ' with "

--- 1768_merge_strings_alternately.py
+++ 1768_merge_strings_alternately.py
@@ -1,11 +1,11 @@
 from itertools import zip_longest
 
 def mergeAlternately(word1, word2):
-    merged = ''.join(a + b for a, b in zip_longest(word1, word2, fillvalue=''))
+    merged = "".join(a + b for a, b in zip_longest(word1, word2, fillvalue=""))
     return merged
 
 def main():
-    print(mergeAlternately('cat', 'pussy'))
+    print(mergeAlternately("cat", "pussy"))
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
